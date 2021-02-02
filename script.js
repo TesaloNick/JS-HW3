8f58d5af3fe03cbe3cd2a28ae271cb45162c9beb
Nikolay Antonovich <TesaloNick@gmail.com>
2021-02-02 22:03:08 +0300

    Task 1

---
+++ script.js
@@ -0,0 +1,9 @@
+document.write('Задание 1. Числа от 1 до 50 и от 35 до 8.<br>');
+for (let i=1; i<=50; i++){
+    document.write(i, ', ');
+}
+document.write('<br>');
+for (let j=35; j>=8; j--){
+    document.write(j, ', ');
+}
+document.write('<br>');
